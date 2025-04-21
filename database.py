d06e906c51f100080321227cb2da940301885a9b
costavalmir <costavalmir2011@gmail.com>
2025-04-21 15:26:29 -0300

    Add files via upload

---
+++ database.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+def criar_tabela():
+    conn = sqlite3.connect("usuarios.db")
+    c = conn.cursor()
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS usuarios (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT NOT NULL,
+            email TEXT NOT NULL,
+            senha TEXT NOT NULL
+        )
+    ''')
+    conn.commit()
+    conn.close()
+
+criar_tabela()
